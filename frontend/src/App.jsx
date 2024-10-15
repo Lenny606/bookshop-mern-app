5c247927e0c47c7e4e139ddf5ef182d703cd1eaa
Tomas Kravcik <root@Laptop-i7>
2024-10-15 12:23:20 +0200

    navbar styles + navlink

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
 import {Outlet} from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
 
-  return (
-    <>
-      <nav>NAvBAR</nav>
-        <main className={'min-h-screen max-w-screen px-4 py-6 mx-auto font-primary'}>
-            <Outlet/>
-        </main>
-      <footer>Footer</footer>
-    </>
-  )
+    return (
+        <>
+            <NavBar/>
+            <main className={'min-h-screen max-w-screen px-4 py-6 mx-auto font-primary'}>
+                <Outlet/>
+            </main>
+            <footer>Footer</footer>
+        </>
+    )
 }
 
 export default App
