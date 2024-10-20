02139ad5cbd08ccf97a7bb4f27fc998e13a6703c
Tomas Kravcik <root@Laptop-i7>
2024-10-20 19:37:45 +0200

    install dotenv, setup db

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
+dotenv.config()
 const app = express();
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL)
+    app.use("/", (req, res) => {
+        res.send('server is running')
+    })
+}
+
+main().then(() => {
+    console.log("DB is connected")
+}).catch(err => console.error(err))
 
 app.listen(port, () => {
 
