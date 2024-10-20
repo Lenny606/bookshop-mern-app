f6241c89c130d36e1a6cd4e5a3ec27db1e472579
Tomas Kravcik <root@Laptop-i7>
2024-10-20 10:36:42 +0200

    redux setup + cartSlice

---
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -0,0 +1,27 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    cartItems: []
+}
+//actions
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            const existingItem = state.cartItems.find(item => item._id === action.payload_id)
+            if (!existingItem) {
+                state.cartItems.push(action.payload)
+                //TODO toast
+                console.log("item added to cart")
+            } else {
+                //TODO toast
+                console.log("item exists")
+            }
+        }
+    }
+})
+
+//export actions
+export const {addToCart} = cartSlice.actions;
+export default cartSlice.reducer;
